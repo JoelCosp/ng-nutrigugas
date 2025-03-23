e7be360a9cf0e2a7dec43d6828fb00ea47e2ee3f
JoelVRV <joelc456789@gmail.com>
2025-03-23 12:56:25 +0100

    UPGRADES COMPONENTES

--- ng-nutrigugas/src/app/app.routes.ts
+++ ng-nutrigugas/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'about', component: AboutMeComponent},
+    {path: 'services', component: ServicesComponent},
+    {path: 'blog', component: BlogComponent},
     {path: '**', component: HomeComponent},
 ];
