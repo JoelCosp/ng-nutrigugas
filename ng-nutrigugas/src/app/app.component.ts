71214031ee609f9905cd4ec3d49ea88905be1392
JoelVRV <joelc456789@gmail.com>
2025-03-23 01:00:40 +0100

    FLOWBITE INSTALADO

--- ng-nutrigugas/src/app/app.component.ts
+++ ng-nutrigugas/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { initFlowbite } from 'flowbite';
+
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-nutrigugas';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
