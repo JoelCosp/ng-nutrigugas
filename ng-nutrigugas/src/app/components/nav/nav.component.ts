8b43cfcbad034a766206d410189c8fee77acf309
JoelVRV <joelc456789@gmail.com>
2025-03-23 22:51:11 +0100

    UPGRADE STYLES

--- ng-nutrigugas/src/app/components/nav/nav.component.ts
+++ ng-nutrigugas/src/app/components/nav/nav.component.ts
@@ -1,12 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  mobile_menu: boolean;
 
+  constructor() {
+    this.mobile_menu = false;
+  }
+
+  toggleBtn(): void {
+    this.mobile_menu = !this.mobile_menu;
+  }
 }
