d51f74ecde2321ecf1b6a00cb2ee1075ae81933c
JoelVRV <joelc456789@gmail.com>
2025-03-23 01:51:39 +0100

    UPGRADE COMPONENTES (info añadida)

--- ng-nutrigugas/src/app/pages/home/home.component.ts
+++ ng-nutrigugas/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { ContactComponent } from '../../components/contact/contact.component';
+import { ServicesComponent } from '../../components/services/services.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, ContactComponent],
+  imports: [HeroComponent, ContactComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
