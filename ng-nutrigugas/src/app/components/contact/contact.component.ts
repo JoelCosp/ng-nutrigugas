4ee8a98baeda76a488224f3217e1dfbc7b8e8f7a
JoelVRV <joelc456789@gmail.com>
2025-03-24 23:24:17 +0100

    ---

--- ng-nutrigugas/src/app/components/contact/contact.component.ts
+++ ng-nutrigugas/src/app/components/contact/contact.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface ContactForm {
+  name: string,
+  email: string,
+  message: string
+}
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
 
+  form: ContactForm = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  send() {
+    console.log(this.form);
+  }
 }
